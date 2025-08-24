f9974631b03594e505f6e366b2d4acff02dfb6f7
Zhong Baode <z5b3d3@gmail.com>
2025-08-24 19:37:56 +0800

    Implemented an initial skeletal version

---
+++ src/main/java/Mochi.java
@@ -0,0 +1,13 @@
+public class Mochi {
+    public static void main(String[] args) {
+        String logo = """
+        ____________________________________________________________
+        Hello! I'm Mochi
+        What can I do for you?
+        ____________________________________________________________
+        Bye. Hope to see you again soon!
+        ____________________________________________________________
+        """;
+        System.out.println("Hello from\n" + logo);
+    }
+}
