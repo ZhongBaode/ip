b5f8205563fd689f5ebeb69c1c276c6559f80140
Zhong Baode <z5b3d3@gmail.com>
2025-09-03 18:03:05 +0800

    Updated main loop to include exit condition with isRunning

--- src/main/java/Mochi.java
+++ src/main/java/Mochi.java
@@ -1,43 +1,52 @@
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.List;
 
 public class Mochi {
 
-    static ArrayList<Task> inputs = new ArrayList<Task>();
+    private static List<Task> inputs = new ArrayList<Task>();   //Apparently linked list can be translated when using List<Task> instead of ArrayList<Task> so it is preferred
+    private boolean isRunning = true;                           //Used to exit the program.
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {                //Java main need to be static, so i cannot call this.isRunning. So i outsource
+        new Mochi().runMain();
+    }
+
+    private void runMain() {
         welcomeMessage();
-        while (true) {
-            String input = sc.nextLine();
-            listenForInputs(input, inputs);
+        try (Scanner sc = new Scanner(System.in)) {
+            while(this.isRunning && sc.hasNextLine()){
+                handle(sc.nextLine());
+            }
+        }
+        goodByeMessage();
+    }
+
+        private static void listenForInputs(String input, ArrayList<Task> inputs) {
+            if (input.trim().equalsIgnoreCase("bye")) {
+                goodByeMessage();
+                System.exit(0);
+            }
+    
+            if (input.trim().equalsIgnoreCase("list")) {
+                printArrayList(inputs);
+            }
+            else if (input.toLowerCase().contains("unmark")){
+                markAsUndone(input, inputs);
+            }
+            else if (input.toLowerCase().contains("mark")){
+                markAsDone(input, inputs);
+            }
+    
+            else{
+                Task task = new Task(input);
+                inputs.add(inputs.size(), task);
+                System.out.println( "added: " + input);
+
         }
     }
 
-    private static void listenForInputs(String input, ArrayList<Task> inputs) {
-        if (input.trim().equalsIgnoreCase("bye")) {
-            goodByeMessage();
-            System.exit(0);
-        }
-
-        if (input.trim().equalsIgnoreCase("list")) {
-            printArrayList(inputs);
-        }
-        else if (input.toLowerCase().contains("unmark")){
-            markAsUndone(input, inputs);
-        }
-        else if (input.toLowerCase().contains("mark")){
-            markAsDone(input, inputs);
-        }
-
-        else{
-            Task task = new Task(input);
-            inputs.add(inputs.size(), task);
-            System.out.println( "added: " + input);
-        }
-    }
-
-    private static void markAsUndone(String input, ArrayList<Task> inputs) {
+    private static void markAsUndone(String input, List<Task> inputs) {
         try {
             int listIndex = Integer.parseInt(input.substring(7)) - 1;
             inputs.get(listIndex).markAsUndone();
@@ -47,7 +56,7 @@ public class Mochi {
         }
     }
 
-    private static void markAsDone(String input, ArrayList<Task> inputs) {
+    private static void markAsDone(String input, List<Task> inputs) {
         try {
             int listIndex = Integer.parseInt(input.substring(5)) - 1;
             inputs.get(listIndex).markAsDone();
@@ -57,7 +66,7 @@ public class Mochi {
         }
     }
 
-    private static void printArrayList(ArrayList<Task> inputs) {
+    private static void printArrayList(List<Task> inputs) {
         System.out.println("____________________________________________________________");
         for(int i = 0; i < inputs.size(); i++) {
             System.out.print(i + 1 + ":[" + inputs.get(i).getStatusIcon() + "]");
